1c1cc89759ac3a41fa18acc15581de3f570a30eb
Nitish Kumar <niralanitrogensir@gmail.com>
2021-08-27 14:31:04 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: LogInPage(),
       home: HomePage(),
+      debugShowCheckedModeBanner: false,//for remove debug banner
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,17 +3,39 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   // const HomePage({ Key? key }) : super(key: key);
   final int day = 2;
-  final String name= "Neelam & Nirala";
+  final String name = "Neelam & Nirala";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar
+      (
+        // toolbarHeight: 200,
+        // toolbarOpacity: 1,
+        // toolbarTextStyle: ,
+        //  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(360)),
+        centerTitle: true,
         title: Text(name),
         backgroundColor: Colors.teal[900],
+
+        // actions: <Widget>[
+        //   IconButton(
+        //     icon: Icon(Icons.comment),
+        //     tooltip: 'Comment Icon',
+        //     onPressed: () {},
+        //   ), //IconButton
+        //   IconButton(
+        //     icon: Icon(Icons.settings),
+        //     tooltip: 'Setting Icon',
+        //     onPressed: () {},
+        //   ), //IconButton
+        // ],
+        
       ),
-      body:Center(
-        child: Container(
+      body: Center
+      (
+        child: Container
+        (
           child: Text("Welcome to $name Home on day $day"),
         ),
       ),
